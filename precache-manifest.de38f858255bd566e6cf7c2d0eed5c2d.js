d225975848f1b4617a93abb2a281d52fd125d89c
HemalDesai <hemald332@gmail.com>
2023-02-28 18:37:30 +0530

    Updates

---
+++ precache-manifest.de38f858255bd566e6cf7c2d0eed5c2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5384d15e031110e8bb5",
+    "url": "/college-marketplace/static/css/main.130993b8.chunk.css"
+  },
+  {
+    "revision": "d5384d15e031110e8bb5",
+    "url": "/college-marketplace/static/js/main.d5384d15.chunk.js"
+  },
+  {
+    "revision": "c1a8d6df04d77bc7c474",
+    "url": "/college-marketplace/static/js/1.c1a8d6df.chunk.js"
+  },
+  {
+    "revision": "b5d91223731316be6db4",
+    "url": "/college-marketplace/static/js/runtime~main.b5d91223.js"
+  },
+  {
+    "revision": "c63e83dce69d608f8a7fc5c0b10686ea",
+    "url": "/college-marketplace/static/media/sellit-logo.c63e83dc.png"
+  },
+  {
+    "revision": "c2037f61c95d6c57d5bb4a1e330da598",
+    "url": "/college-marketplace/index.html"
+  }
+];
